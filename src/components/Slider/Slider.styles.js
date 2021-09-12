926422a57b67c906d2b3eaf253b04c4b8ffafa8b
Sonlq2001 <sonweb2001@gmail.com>
2021-09-12 22:12:39 +0700

    slider, layout grid

---
+++ src/components/Slider/Slider.styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const SliderMain = styled.div`
+  overflow: hidden;
+`;
+
+export const SliderItem = styled.div`
+  width: 100%;
+  /* max-height: 50rem; */
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
