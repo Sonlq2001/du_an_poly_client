b7d5ac222b9d158482086922a8d3aea61cf7ea3f
Sonlq2001 <sonweb2001@gmail.com>
2021-09-14 22:27:34 +0700

    relsove branch

--- src/features/about/screens/AboutScreen/AboutScreen.jsx
+++ src/features/about/screens/AboutScreen/AboutScreen.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const AboutScreen = () => {
-  return <div>about page</div>
-}
+  return <div className="container">le quang son</div>;
+};
 
-export default AboutScreen
+export default AboutScreen;
