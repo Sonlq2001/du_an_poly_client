bce358a97e285ae02300e6f85f2a692bafe6cd0c
son2210 <z0zb0yz0z@gmail.com>
2021-09-21 00:18:51 +0700

    fig lại page profile

--- src/features/profile/components/profile.jsx
+++ src/features/profile/components/profile.jsx
@@ -1,20 +1,40 @@
 import React from 'react';
 import { HeaderProfile, Avatar, Information } from './style';
+import { Link } from 'react-router-dom';
 const Profile = () => {
   return (
     <HeaderProfile className="profile">
       <Avatar className="avatar">
         <img
-          src="https://i.pinimg.com/474x/b8/d0/ca/b8d0ca1517e1159556efac1a89a7739d.jpg"
+          src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
           alt=""
         />
       </Avatar>
       <Information className="information">
-        <ul>
+        <div className="left">
+          <li>Họ và Tên </li>
+          <li>Email </li>
+          <li> Mssv </li>
+          <li>Chuyên ngành </li>
+          <li>Link git </li>
+          <li>Cv cá nhân </li>
+        </div>
+        <div className="rigth">
+          <li>Nguyễn Hữu Sơn </li>
+          <li>Sonnhph12562@fpt.edu.vn </li>
+          <li> Ph12562 </li>
+          <li> Lập trình website </li>
           <li>
-            <h3>Nguyễn Hữu Sơn</h3>{' '}
+            <a href="https://github.com/" target="_blank">
+              https://github.com/
+            </a>
           </li>
-        </ul>
+          <li>
+            <a href="https://www.topcv.vn/cv-la-gi" target="_blank">
+              https://www.topcv.vn/cv-la-gi
+            </a>
+          </li>
+        </div>
       </Information>
     </HeaderProfile>
   );
--- src/features/profile/components/style.js
+++ src/features/profile/components/style.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { AiOutlineEye } from 'react-icons/ai';
 export const HeaderProfile = styled.div`
   &.profile {
     display: grid;
@@ -8,21 +9,43 @@ export const HeaderProfile = styled.div`
     box-sizing: border-box;
   }
 `;
+export const Eye = styled(AiOutlineEye)`
+  color: #ed8936;
+  align-items: center;
+  font-size: 14px;
+  margin-right: 8px;
+`;
 export const Avatar = styled.div`
   padding-right: 10px;
   & img {
     float: right;
     border-radius: 50%;
-    max-height: 30%;
+    max-height: 130px;
+    width: 110px;
   }
 `;
 export const Information = styled.div`
   padding-left: 20px;
-  & ul li {
+  display: flex;
+  & li {
     list-style: none;
+    padding: 5px 2px;
+    font-size: 1.5rem;
+    font-weight: 550;
   }
-  & ul li h3 {
-    font-size: 16px;
+  .left {
+    padding: 0px 10px 0 0;
+  }
+  .rigth li {
+    font-size: 1.5rem;
+    font-weight: 570;
+    padding-left: 10px;
+  }
+  .rigth li a {
+    text-decoration: none;
+    /* color: #28287bf7; */
+    color: #374151;
+    font-weight: 550;
   }
 `;
 // end profile
@@ -38,14 +61,28 @@ export const Title = styled.div`
 `;
 export const Product = styled.div`
   box-sizing: border-box;
+
   padding: 10px 0px;
   & .productItem {
     padding: 10px;
+    overflow: hidden;
+    box-sizing: border-box;
   }
   & .productItem img {
     width: 100%;
     height: 230px;
+    overflow: hidden;
     border-radius: 5px;
+    transform: scale(0.98);
+    transition: 1s;
+  }
+  & .productItem img:hover {
+    transform: scale(1.03);
+  }
+  .productItem a {
+    color: #374151;
+    text-decoration: none;
+    cursor: pointer;
   }
   & .contenPro {
     padding: 10px 2px 0px 2px;
@@ -53,13 +90,13 @@ export const Product = styled.div`
     justify-content: space-between;
   }
   & .contenPro .title {
-    font-size: 17px;
-    font-weight: 550;
+    font-size: 1.5rem;
+    font-weight: 450;
     color: #374151;
   }
   & .contenPro .view {
-    font-size: 13px;
-    font-weight: 600;
+    font-size: 14px;
+    font-weight: 450;
     color: #374151;
     display: flex;
   }
