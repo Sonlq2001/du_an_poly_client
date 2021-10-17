643d598a10ccca0446328fa63f4acee5c6040318
Sonlq2001 <sonweb2001@gmail.com>
2021-10-17 16:25:34 +0700

    add redirect route

--- src/features/add-product/routes/add-product.routes.js
+++ src/features/add-product/routes/add-product.routes.js
@@ -5,5 +5,6 @@ const ADD_PRODUCT_SCREEN = {
   id: 'id_product_add',
   path: '/product/add',
   component: AddProductScreen,
+  isPrivateRoute: true,
 };
 export const ADD_PRODUCT_ROUTES = [ADD_PRODUCT_SCREEN];
