dc887ee1753962f760f23440e081c9429f2f6d8f
Sonlq2001 <sonweb2001@gmail.com>
2021-12-22 00:06:49 +0700

    fix get profile user

--- src/features/profile/api/profile.api.js
+++ src/features/profile/api/profile.api.js
@@ -2,14 +2,9 @@ import api from 'api/api';
 
 import { PROFILE_ENDPOINTS } from '../constants/profile.endpoints';
 
-const getProductsUser = (id) => {
+const getProfileUser = (id) => {
   return api.get(PROFILE_ENDPOINTS.GET_LIST_PRODUCT_USER.replace(':id', id));
 };
-
-const getDetails = (id) => {
-  return api.get(PROFILE_ENDPOINTS.GET_PROFILE_USER.replace(':id', id));
-};
 export const profileApi = {
-  getProductsUser,
-  getDetails,
+  getProfileUser,
 };
